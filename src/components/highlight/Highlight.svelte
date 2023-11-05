c890e9ddbc82aa301039e94d441adb8886a339a3
dimaslz <me@dimaslz.io>
2023-11-05 18:33:08 +0100

    feat(highlight/components): add hightlight code component

---
+++ src/components/highlight/Highlight.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import "./theme.css";
+	import "./prism-highlight-code";
+
+	import Prism from "prismjs";
+
+	export let code: string = "";
+
+	let html: string = "";
+	html = Prism.highlight(code, Prism.languages.svelte, "svelte");
+</script>
+
+<div>
+	<div class="p-4 bg-gray-950">
+		<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+		<pre class="text-xs overflow-x-scroll"><code>{@html `${html.trim()}\n\n`}</code></pre>
+	</div>
+</div>
