30dbc654651e9b391e7435cc0b3be13f4bec78b6
dimaslz <me@dimaslz.io>
2024-03-05 22:50:13 +0100

    chore(components): remove prismjs in favor of highlightjs

--- src/components/highlight/Highlight.svelte
+++ src/components/highlight/Highlight.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import "./theme.css";
-	import "./prism-highlight-code";
-
-	import Prism from "prismjs";
+	import Highlight, { HighlightSvelte } from "svelte-highlight";
+	import typescript from "svelte-highlight/languages/typescript";
+	import codeStyle from "svelte-highlight/styles/github-dark";
 
 	import { ClipboardOutlineIcon } from "@/components/icons";
 	import { useClipboard, useDebounceFn } from "@/hooks";
 
 	export let code: string = "";
+	export let language: string = "svelte";
 	let showConfirmation: boolean = false;
 
 	const [, copyClipboard] = useClipboard();
@@ -23,13 +23,15 @@
 		showConfirmation = true;
 		hideConfirmationDebounced();
 	};
-
-	let html: string = "";
-	html = Prism.highlight(code, Prism.languages.svelte, "svelte");
 </script>
 
+<svelte:head>
+	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+	{@html codeStyle}
+</svelte:head>
+
 <div>
-	<div class="p-4 bg-gray-950 relative">
+	<div class="bg-gray-950 relative text-xs">
 		{#if showConfirmation}
 			<div class="absolute left-2 top-2 bg-green-900 text-xs p-2">code copied!</div>
 		{/if}
@@ -41,7 +43,10 @@
 			<ClipboardOutlineIcon class="w-4 h-4 text-gray-600" />
 		</button>
 
-		<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-		<pre class="text-xs overflow-x-scroll"><code>{@html `${html.trim()}`}</code></pre>
+		{#if language === "typescript"}
+			<Highlight language={typescript} code={code.trim()} />
+		{:else}
+			<HighlightSvelte code={code.trim()} />
+		{/if}
 	</div>
 </div>
