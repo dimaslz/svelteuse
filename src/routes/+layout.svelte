40a01e7f12aa296e906cbcb180962501325cc3d4
dimaslz <me@dimaslz.io>
2024-05-01 23:51:01 +0200

    fix(head): add social network meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,6 @@
 	import "../app.scss";
 </script>
 
-<svelte:head>
-	<title>Svelte hook react like - by @dimaslz</title>
-	<meta name="description" content="Dimas López resume - @dimaslz" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-</svelte:head>
-
 <div class="flex flex-col items-center w-full h-full">
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,12 +19,6 @@
 	};
 </script>
 
-<svelte:head>
-	<title>Svelte hook react like - by @dimaslz</title>
-	<meta name="description" content="Dimas López resume - @dimaslz" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-</svelte:head>
-
 <div class="flex flex-col h-screen items-center w-full">
 	<Header />
 
