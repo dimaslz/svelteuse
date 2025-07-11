5e9bf310f9e74f1b4b8db2f24fe73ca78ef304aa
dimaslz <me@dimaslz.io>
2025-07-12 00:06:55 +0100

    tests(hooks/useWindowSize): fix test on resize

--- src/hooks/useWindowSize/useWindowSize.test.ts
+++ src/hooks/useWindowSize/useWindowSize.test.ts
@@ -14,39 +14,43 @@ vi.mock("esm-env", async (importOriginal) => {
 });
 
 describe("Hooks - useWindowSize", () => {
-	test("without a component should works", () => {
-		const { windowSize } = useWindowSize();
+  describe("test hook", () => {
+    test("without a component should works", () => {
+      const { windowSize } = useWindowSize();
 
-		expect(get(windowSize)).toEqual({
-			height: 768,
-			width: 1024,
-		});
+      expect(get(windowSize)).toEqual({
+        height: 768,
+        width: 1024,
+      });
 
-		window.innerWidth = 1000;
-		window.innerHeight = 1000;
+      window.innerWidth = 1000;
+      window.innerHeight = 1000;
 
-		window.dispatchEvent(new Event("resize"));
+      window.dispatchEvent(new Event("resize"));
 
-		expect(get(windowSize)).toEqual({
-			height: 1000,
-			width: 1000,
-		});
-	});
+      expect(get(windowSize)).toEqual({
+        height: 1000,
+        width: 1000,
+      });
+    });
+  });
 
-	test("into a component should works", () => {
-		const { rerender } = render(UseWindowSize);
+  describe("test in component", () => {
+  	test("into a component should works", async () => {
+  		const { rerender } = render(UseWindowSize);
 
-		expect(screen.getByText(/width: 1000/i));
-		expect(screen.getByText(/height: 1000/i));
+  		expect(screen.getByText(/width: 1000/i));
+  		expect(screen.getByText(/height: 1000/i));
 
-		window.innerWidth = 2000;
-		window.innerHeight = 2000;
+  		window.innerWidth = 2000;
+  		window.innerHeight = 2000;
 
-		window.dispatchEvent(new Event("resize"));
+  		window.dispatchEvent(new Event("resize"));
 
-		rerender({});
+  		await rerender({});
 
-		expect(screen.getByText(/width: 2000/i));
-		expect(screen.getByText(/height: 2000/i));
-	});
+  		expect(screen.getByText(/width: 2000/i));
+  		expect(screen.getByText(/height: 2000/i));
+  	});
+  });
 });
